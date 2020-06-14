d95c15d27614da6f2a343b09add5ee04686055e3
Erik Yuzwa <erikyuzwa@gmail.com>
2020-06-13 20:59:18 -0600

    set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :todo_lists
+
+  # our homepage will be the list of todo items
+  root "todo_lists#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
