0b02e2ed164006e9f2ae46e066cc3ea8d22c08e7
Erik Yuzwa <erikyuzwa@gmail.com>
2020-06-13 21:06:20 -0600

    update controller

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,2 +1,25 @@
 class TodoItemsController < ApplicationController
+  # tap into before_action hook to find our proper todo_list
+  before_action :set_todo_list
+  
+  # create method run from POST
+  def create
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
+    redirect_to @todo_list
+  end
+
+  private
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
+  def todo_item_params
+    params[:todo_item].permit(:content)
+  end
+
 end
