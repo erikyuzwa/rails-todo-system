cec9be96700ccc5fe5bcdd14d1c295c2e7e72d6d
Erik Yuzwa <erikyuzwa@gmail.com>
2020-06-13 21:14:58 -0600

    update with destroy method

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -8,6 +8,16 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "item was deleted"
+    else
+      flash[:error] = "item could not be deleted"
+    end
+    redirect_to @todo_list
+  end
+
   private
 
   def set_todo_list
