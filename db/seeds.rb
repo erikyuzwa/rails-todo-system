e5d8f8a29f3cab62446ae05cfa2b39c0759d25dc
Erik Yuzwa <erikyuzwa@gmail.com>
2020-06-15 06:51:13 -0600

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+list1 = TodoList.create(title: 'Shopping List', description: 'Things to grab during the day')
+TodoItem.create(content: 'milk 2%', todo_list: list1)
+TodoItem.create(content: 'rolled oats', todo_list: list1)
+
+list2 = TodoList.create( title: 'Work List', description: 'Things to finish today at work')
+TodoItem.create(content: 'prepare release notes', todo_list: list2)
+TodoItem.create(content: 'update source', todo_list: list2)
